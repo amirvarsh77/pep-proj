5077937c67a9dfc283a558b69226b0126f83161c
amirvarsh77 <amirvarsh77@gmail.com>
2026-01-05 23:08:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const forms = document.querySelectorAll(".email-signup");
+
+forms.forEach(form => {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault(); // stop page reload
+
+        const emailInput = form.querySelector("input[type='email']");
+
+        if (emailInput.value.trim() === "") {
+            alert("Please enter your email address");
+        } else {
+            alert("Thanks! This is a demo Netflix clone project -");
+            emailInput.value = "";
+        }
+    });
+});
